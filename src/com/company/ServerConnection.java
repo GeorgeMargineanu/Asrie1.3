872a9aea5fa04c07be400eb0cf8f0a7a1548da9b
georg <georgemargineanu20@gmail.com>
2020-04-20 16:16:22 +0200

    First commit

--- src/com/company/ServerConnection.java
+++ src/com/company/ServerConnection.java
@@ -3,8 +3,18 @@ package com.company;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ServerConnection implements Runnable {
+
+    String baseDirectory = "C:\\Users\\georg\\Desktop\\Asrie1.3\\FilesToRead\\";
+    String inputFile = "Input.txt";
+    String orderFile = "Orders.txt";
+    String beaconFile = "Beacon.txt";
+    String outputFile = "Output.txt";
+    String companyFile = "Company.txt";
+    String phraseologyFile = "Phraseology.txt";
+    HashMap<String , String> map = new HashMap< String, String>();
 
     private Socket server;
     BufferedReader ois;
@@ -22,11 +32,29 @@ public class ServerConnection implements Runnable {
     {
             try {
                 while (true) {
+
+                    map.put("ZERO", "0");
+                    map.put("ONE", "1");
+                    map.put("TWO", "2");
+                    map.put("THREE","3");
+                    map.put("FOUR", "4");
+                    map.put("FIVE", "5");
+                    map.put("SIX", "6");
+                    map.put("SEVEN", "7");
+                    map.put("EIGHT", "8");
+                    map.put("NINE", "9");
+                    map.put("ONE HUNDRED", "100");
+                    map.put("ONE THOUSAND", "1000");
+
+
                     System.out.println("Client Addres: " + address);
                     String line = "hi server";
                    // oos.println(line);
                     oos.flush();
                     String response = ois.readLine();
+
+
+
                     System.out.println(response);
                }
             }
